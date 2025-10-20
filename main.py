3324dd822f5954ba637324f7757b5e5a3d620776
Pho Hale <PhoHale@gmail.com>
2025-10-20 11:59:59 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from phopylslhelper!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/phopylslhelper/easy_time_sync.py
@@ -0,0 +1,94 @@
+from typing import Dict, List, Tuple, Optional, Callable, Union, Any
+from datetime import datetime, timedelta
+import pytz
+import pylsl
+
+
+def readable_dt_str(a_dt: datetime, tz: pytz.timezone = pytz.timezone("US/Eastern")) -> str:
+    """ returns the datetime in a readible string format """
+    return str(a_dt.astimezone(tz).strftime("%Y-%m-%d %I:%M:%S %p"))
+
+
+def from_readable_dt_str(a_dt_str: str, tz: pytz.timezone = pytz.timezone("US/Eastern")) -> datetime:
+    """ Inverse of `readable_dt_str(...)` """
+    return datetime.strptime(a_dt_str, "%Y-%m-%d %I:%M:%S %p").replace(tzinfo=tz)
+
+
+class EasyTimeSyncParsingMixin:
+    """
+    self.stream_start_lsl_local_offset = None
+    self.stream_start_datetime = None
+    
+    Usage:
+    
+            from phopylslhelper.easy_time_sync import EasyTimeSyncParsingMixin, readable_dt_str, from_readable_dt_str
+                        
+            
+            ## In class __init__:
+                self.common_capture_stream_start_timestamps() ## `EasyTimeSyncParsingMixin`: capture timestamps for use in LSL streams
+
+                                        
+    """
+
+    @property
+    def stream_start_datetime(self) -> datetime:
+        return self.arbitrary_time_sync_points["stream_start"][0]
+
+    @property
+    def stream_start_lsl_local_offset(self) -> float:
+        return self.arbitrary_time_sync_points["stream_start"][1]
+
+
+    @property
+    def recording_start_datetime(self) -> datetime:
+        return self.arbitrary_time_sync_points["recording_start"][0]
+
+    @property
+    def recording_start_lsl_local_offset(self) -> float:
+        return self.arbitrary_time_sync_points["recording_start"][1]
+        
+    @property
+    def arbitrary_time_sync_points(self) -> Dict[str, Tuple[datetime, float]]:
+        return self._arbitrary_time_sync_points        
+
+
+    def init_EasyTimeSyncParsingMixin(self):
+        # self._stream_start_lsl_local_offset = None
+        # self._stream_start_datetime = None
+        # self._recording_start_lsl_local_offset = None
+        # self._recording_start_datetime = None
+        self._arbitrary_time_sync_points = {}
+
+        self.capture_stream_start_timestamps()
+
+
+    def add_arbitrary_time_sync_point(self, label: str, dt: datetime, lsl_local_offset: float):
+        """ Add an arbitrary time sync point for later reference """
+        self._arbitrary_time_sync_points[label] = (dt, lsl_local_offset)
+
+
+    def capture_current_arbitrary_time_sync_point(self, label: str):
+        """ Capture the current time as an arbitrary time sync point """
+        current_lsl_local_offset = pylsl.local_clock()
+        current_datetime = datetime.now(datetime.timezone.utc)
+        self.add_arbitrary_time_sync_point(label, current_datetime, current_lsl_local_offset)
+    
+
+
+    def capture_stream_start_timestamps(self):
+        """ Capture recording start timestamps for use in LSL streams """
+        # Capture the local time offset between LSL and system clock
+        # self._stream_start_lsl_local_offset = pylsl.local_clock()
+        # Capture the current datetime with timezone info
+        # self._stream_start_datetime = datetime.now(datetime.timezone.utc)
+        self.capture_current_arbitrary_time_sync_point("stream_start")
+
+
+    def capture_recording_start_timestamps(self):
+        """ Capture recording start timestamps for use in LSL streams """
+        self.capture_current_arbitrary_time_sync_point("recording_start")
+
+
+
+
+
